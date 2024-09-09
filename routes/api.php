de5d74a29c26abf065c55862dd0ef5405e19ca72
MilesChou <jangconan@gmail.com>
2024-09-09 23:04:56 +0800

    Add filter and tail sampling

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/health', function () {
+    return \Illuminate\Support\Facades\Response::json(['result' => 'ok']);
+});
+
+Route::get('/hello', function () {
+    return \Illuminate\Support\Facades\Response::json(['result' => 'hello']);
+});
